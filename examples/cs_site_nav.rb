27bc661e735778293e52a1471828b0d02c3894c1
Don Voita <don@engineering.ucsb.edu>
2013-05-04 21:37:27 -0700

    Added xpath example

--- examples/cs_site_nav.rb
+++ examples/cs_site_nav.rb
@@ -9,7 +9,17 @@ require 'open-uri'
 
 doc = Nokogiri::HTML(open("http://www.cs.ucsb.edu"))
 
+# CSS selector example
 nav = doc.css("div#site_nav")
 nav.css('ul li').each do |h|
   p h.text
 end
+
+puts ""
+
+# XPath selector example
+nav = doc.xpath('id("site_nav")')
+ nav.xpath('ul/li').each do |h|
+  p h.text
+end
+
